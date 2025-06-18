1d4cb1d17f5567a8e72390462755c584c6aa9dc9
Victot <victorfarian@gmail.com>
2025-06-18 10:35:38 -0300

    definindo os pacotes iniciais do projeto

---
+++ todo_list_topaz/src/main/java/com/victot_exe/todo_list_topaz/TodoListTopazApplication.java
@@ -0,0 +1,13 @@
+package com.victot_exe.todo_list_topaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListTopazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListTopazApplication.class, args);
+	}
+
+}
---
+++ todo_list_topaz/src/test/java/com/victot_exe/todo_list_topaz/TodoListTopazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victot_exe.todo_list_topaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListTopazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
