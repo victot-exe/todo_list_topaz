f33b618dbb05418c6a0573cf89f3b10da1d211b0
Victot <victorfarian@gmail.com>
2025-06-19 01:01:03 -0300

    adicionar arquivos do front e estrutura básica

---
+++ todo_list_topaz_front/script.js
@@ -0,0 +1,16 @@
+//TODO metodo que verfica se tem alguma tarefa, se não ele já abre a tela para inserir pelo menos uma
+
+//TODO metodo para popular e preecher as as divisões
+
+//TODO método de clicar na tarefa e fazer a mesma ficar grandona
+
+//TODO método que cria tarefa
+
+//TODO método que edita tarefa
+
+//TODO metodo que exclui tarefa
+
+//TODO arrasta e solta para mover a tarefa de lugar
+
+
+console.log("Hello world!");
